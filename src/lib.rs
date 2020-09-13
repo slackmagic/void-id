2043c6b4ce5389390827660db29988a4023547f3
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-09-13 12:50:27 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -19,29 +19,34 @@ pub enum ObjectIdError {
     CastFromStringImpossibleSize,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ObjectId {
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct VOid {
     pub seed: Seed,
 }
 
-impl ObjectId {
+impl VOid {
+    #[inline]
+    pub fn new_empty() -> Self {
+        VOid::default()
+    }
+
     #[inline]
     pub fn new() -> Result<Self, ObjectIdError> {
         let mut random = [0u8; RANDOM_HASH_SIZE];
         match getrandom(&mut random) {
-            Ok(_) => Ok(ObjectId { seed: random }),
+            Ok(_) => Ok(VOid { seed: random }),
             Err(_) => Err(ObjectIdError::GenerationImpossible),
         }
     }
 
     #[inline]
     pub fn from_string(id: String) -> Result<Self, ObjectIdError> {
-        match ObjectId::hex_string_to_vec(&id) {
+        match VOid::hex_string_to_vec(&id) {
             Ok(vec) => match vec.len() == RANDOM_HASH_SIZE {
                 true => {
                     let mut seed = [0u8; RANDOM_HASH_SIZE];
                     seed.clone_from_slice(vec.as_slice());
-                    Ok(ObjectId { seed: seed })
+                    Ok(VOid { seed: seed })
                 }
                 false => Err(ObjectIdError::CastFromStringImpossibleSize),
             },
@@ -57,14 +62,14 @@ impl ObjectId {
     }
 }
 
-impl AsRef<[u8]> for ObjectId {
+impl AsRef<[u8]> for VOid {
     #[inline]
     fn as_ref(&self) -> &[u8] {
         &self.seed
     }
 }
 
-impl ToString for ObjectId {
+impl ToString for VOid {
     #[inline(always)]
     fn to_string(&self) -> String {
         format!("{:02x}", self.seed.as_ref().iter().format(""))
@@ -76,7 +81,7 @@ mod tests {
     use super::*;
     #[test]
     fn generate_id() {
-        let id = ObjectId::new().unwrap();
+        let id = VOid::new().unwrap();
         println!("{:?}", id.to_string());
         println!("{:?}", id.to_string().len());
         assert_eq!(id.seed.len(), RANDOM_HASH_SIZE);
@@ -87,7 +92,7 @@ mod tests {
         let digest: String =
             "74dc1afb9a999c65b40e8e8d20cfa5651a6178b0280397419271fdc86439f6e2".to_owned();
 
-        let id = ObjectId::from_string(digest.clone()).unwrap();
+        let id = VOid::from_string(digest.clone()).unwrap();
         assert_eq!(id.to_string(), digest.to_owned());
         assert_eq!(id.seed.len(), 32);
     }
